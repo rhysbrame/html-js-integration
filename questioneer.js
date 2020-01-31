5f008ce171f79b8d39a4b7b92ea29124216d86c7
Rhys Brame <rhysbrame@rhyss-mbp.lan>
2020-01-31 16:10:24 +0000

    some javascript testing

--- questioneer.js
+++ questioneer.js
@@ -1,1 +1,7 @@
-prompt("what's your name?");
+var name = prompt("what's your name?");
+
+var age = prompt("how old are you");
+
+console.log("Your name is " + name + " and your age is " + age + ".");
+
+console.log("You have been alive for " + age * 365 + " days!!");
